a192705eea3e4ab55ba6a8ea9a74639f85f10c35
koushik <koushik.koushik.kumar@gmail.com>
2021-01-12 12:18:41 +0530

    Server.JSON not working

--- src/Lib.hs
+++ src/Lib.hs
@@ -4,80 +4,27 @@
 
 module Lib where
 
--- import GHC.Generics (Generic)
--- import qualified Data.Aeson as Aeson
--- import qualified Zero.Server as Server
-
--- helloHandler :: Server.Handler
--- helloHandler =
---   Server.simpleHandler Server.GET "/hello" helloCallback
-
--- data Item = Item {model :: String, quantity :: Int}
---   deriving (Eq, Show, Generic, Aeson.FromJSON, Aeson.ToJSON)
-
--- data Cart
---   = Cart [Item]
---   deriving (Eq, Show, Generic, Aeson.FromJSON, Aeson.ToJSON)
-
--- helloCallback :: Server.Request -> Server.Response
--- helloCallback req =
---   Server.stringResponse "Hello World"
-
--- initialState :: Cart
--- initialState = Cart []
-
--- addToCartHandler :: Server.Request -> Server.Response
--- addToCartHandler req = Server.jsonResponse (Server.requestBody req)
-
 import qualified Data.Aeson as Aeson
 import Data.Text
 import GHC.Generics (Generic)
 import qualified Zero.Server as Server
 
-data Item = Item {model :: String, quantity :: Int}
-  deriving (Eq, Show, Generic)
+data Person = Person {name :: String, age :: Int}
+  deriving (Generic, Server.FromJSON, Server.ToJSON)
 
-instance FromJSON Item where
-  parseJSON (Object v) = Item <$> v .: "model" <*> v .: "quantity"
-  parseJSON _ = empty
-
-instance ToJSON Item where
-  toJSON (Item model quantity) = object ["model" .= model, "quantity" .= quantity]
-
-data Cart
-  = Cart [Item]
-  deriving (Eq, Show, Generic)
-
-addToCartHandler :: Cart -> Server.Request -> (Cart, Server.Response)
-addToCartHandler (Cart cart) req =
-  case Server.decodeJson body of
-    Right item ->
-      (appendToCart item, Server.stringResponse "ok")
-    Left err ->
-      (Cart cart, Server.failureResponse $ "Got an error! " ++ show err)
+myHandler :: Server.Request -> Server.Response
+myHandler req =
+  stringResponse result
   where
     body =
-      Server.requestBody req
-
-    appendToCart item =
-      Cart (cart ++ [item]) -- append item at the end of the list
-
-currentCartHandler :: Cart -> Server.Request -> (Cart, Server.Response)
-currentCartHandler state _ =
-  (state, response)
-  where
-    response =
-      Server.jsonResponse state
-
-initialState :: Cart
-initialState = Cart []
+      requestBody req
+    result =
+      case decodeJson body of
+        Left err -> "Failed to decode request body as a Person. It must be something else"
+        Right p -> "Yay! We have a person named: " <> (name p)
 
 run :: IO ()
 run =
   Server.startServer
-    [ Server.handlersWithState
-        initialState
-        [ Server.statefulHandler Server.POST "/cart" addToCartHandler,
-          Server.statefulHandler Server.GET "/cart" currentCartHandler
-        ]
+    [ Server.simpleHandler Server.POST "/person" myHandler
     ]
